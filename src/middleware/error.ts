b8c80ca435eb2dff94a3c433fbf181e5d98ec1e1
Luis <luisperi18@hotmail.com>
2023-06-13 20:05:53 +0200

    Add sauce mongo repo

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,12 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '../types/http.error';
 
+import createDebug from 'debug';
+const debug = createDebug('W6:ErrorMiddleware');
 export const errorHandler = (
   error: Error,
   _req: Request,
   res: Response,
   _next: NextFunction
 ) => {
+  debug('Executed');
+
   if (error instanceof HttpError) {
     console.error(error.status, error.statusMessage, error.message);
     res.status(error.status);
---
+++ src/repository/sauce.mongo.repository.ts
@@ -0,0 +1,44 @@
+import { SauceModel } from './sauce.mongo.model.js';
+import createDebug from 'debug';
+import { Sauce } from '../entities/sauce.js';
+import { Repo } from './repo.js';
+import { HttpError } from '../types/http.error.js';
+const debug = createDebug('W6:BookRepo');
+
+export class SauceRepo implements Repo<Sauce> {
+  constructor() {
+    debug('Instantiated');
+  }
+
+  async query(): Promise<Sauce[]> {
+    const aData = await SauceModel.find().exec();
+    return aData;
+  }
+
+  async queryById(id: string): Promise<Sauce> {
+    const result = await SauceModel.findById(id).exec();
+    if (result === null)
+      throw new HttpError(404, 'Not found', 'Bad id for the query');
+    return result;
+  }
+
+  async create(data: Omit<Sauce, 'id'>): Promise<Sauce> {
+    const newBook = await SauceModel.create(data);
+    return newBook;
+  }
+
+  async update(id: string, data: Partial<Sauce>): Promise<Sauce> {
+    const newBook = await SauceModel.findByIdAndUpdate(id, data, {
+      new: true,
+    }).exec();
+    if (newBook === null)
+      throw new HttpError(404, 'Not found', 'Bad id for the update');
+    return newBook;
+  }
+
+  async delete(id: string): Promise<void> {
+    const result = await SauceModel.findByIdAndDelete(id).exec();
+    if (result === null)
+      throw new HttpError(404, 'Not found', 'Bad id for the delete');
+  }
+}
