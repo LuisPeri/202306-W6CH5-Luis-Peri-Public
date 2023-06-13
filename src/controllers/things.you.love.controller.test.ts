f975e51afa73b4073d75f6b5a900e9d56a7530e8
Luis <luisperi18@hotmail.com>
2023-06-13 23:06:25 +0200

    Add changes to test things controller

--- src/controllers/things.you.love.controller.test.ts
+++ src/controllers/things.you.love.controller.test.ts
@@ -3,22 +3,22 @@ import { ThingsYouLoveRepo } from '../repository/things.you.love.repository';
 import { ThingsYouLoveController } from './things.you.love.controller';
 
 describe('Given ThigsYouLoveController class', () => {
+  const mockRepo: ThingsYouLoveRepo = {
+    query: jest.fn(),
+    queryById: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+  const req = {
+    params: { id: 1 },
+  } as unknown as Request;
+  const res = {
+    send: jest.fn(),
+  } as unknown as Response;
+  const next = jest.fn() as NextFunction;
+  const controller = new ThingsYouLoveController(mockRepo);
   describe('When it is instantiated', () => {
-    const mockRepo: ThingsYouLoveRepo = {
-      query: jest.fn(),
-      queryById: jest.fn(),
-      create: jest.fn(),
-      update: jest.fn(),
-      delete: jest.fn(),
-    };
-    const req = {
-      params: { id: 1 },
-    } as unknown as Request;
-    const res = {
-      send: jest.fn(),
-    } as unknown as Response;
-    const next = jest.fn() as NextFunction;
-    const controller = new ThingsYouLoveController(mockRepo);
     test('Then method getAll should be used', async () => {
       await controller.getAll(req, res, next);
       expect(res.send).toHaveBeenCalled();
